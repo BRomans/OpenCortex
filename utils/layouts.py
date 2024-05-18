1ea670179364a8ba206efaaa40d7dd14af39e64c
Michele Romani <michele.romani.gzl0@gmail.com>
2024-05-18 12:21:28 +0200

    :sparkles: Add SYNTHETIC_BOARD as default, updated layouts, minimum width for plots and minor improvements to the codebase.

--- utils/layouts.py
+++ utils/layouts.py
@@ -1,11 +1,24 @@
 from brainflow import BoardIds
 
 layouts = {
+    BoardIds.SYNTHETIC_BOARD.value: {
+        "channels": ["channel_1", "channel_2", "channel_3", "channel_4", "channel_5", "channel_6", "channel_7",
+                     "channel_8", "channel_9", "channel_10", "channel_11", "channel_12", "channel_13", "channel_14",
+                     "channel_15", "channel_16"],
+        "header": ["channel_1", "channel_2", "channel_3", "channel_4", "channel_5", "channel_6", "channel_7",
+                   "channel_8", "channel_9", "channel_10", "channel_11", "channel_12", "channel_13", "channel_14",
+                   "channel_15", "channel_16", "Trigger"],
+        "eeg_start": 0,
+        "eeg_end": 16
+    },
     BoardIds.UNICORN_BOARD.value: {
         "channels": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8"],
-        "header": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8", "AccX", "AccY", "AccZ", "GyroX", "GyroY", "GyroZ", "CNT", "BAT", "VALID", "DeltaTime", "Trigger"],
-        "raw": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8", "AccX", "AccY", "AccZ", "GyroX", "GyroY", "GyroZ", "CNT", "BAT", "VALID", "DeltaTime", "Trigger"],
-        "unity": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8", "trigger", "id", "target", "nontarget", "trial", "islast"],
+        "header": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8", "AccX", "AccY", "AccZ", "GyroX", "GyroY", "GyroZ",
+                   "CNT", "BAT", "VALID", "DeltaTime", "Trigger"],
+        "raw": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8", "AccX", "AccY", "AccZ", "GyroX", "GyroY", "GyroZ",
+                "CNT", "BAT", "VALID", "DeltaTime", "Trigger"],
+        "unity": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8", "trigger", "id", "target", "nontarget", "trial",
+                  "islast"],
         "slim": ["Fz", "C3", "Cz", "C4", "Pz", "PO7", "Oz", "PO8", "STI"],
         "eeg_start": 0,
         "eeg_end": 8,
