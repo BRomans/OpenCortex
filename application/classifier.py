:sparkles: compute class from proba, normalization of probabilities, fix serialization for LSL output.

application/classifier.py
@@ -1,6 +1,7 @@
 import logging
 import mne.utils
 import numpy as np
+import matplotlib
 from brainflow import BoardShim, BoardIds
 from imblearn.over_sampling import RandomOverSampler
 from mne import set_eeg_reference, find_events, Epochs
@@ -12,11 +13,11 @@ from matplotlib import pyplot as plt
 from utils.layouts import layouts
 from utils.loader import convert_to_mne
 from utils.preprocessing import basic_preprocessing_pipeline, extract_epochs, make_overlapping_epochs
-from utils.validation import plot_cross_validated_roc_curve, plot_cross_validated_confusion_matrix
+from utils.validation import plot_cross_validated_roc_curve, plot_cross_validated_confusion_matrix, normalize
 
 # turn off MNE logging
 mne.utils.set_log_level('ERROR')
-#matplotlib.use("Qt5Agg")
+matplotlib.use("Qt5Agg")
 random_state = 32
 np.random.seed(random_state)
 
@@ -100,11 +101,11 @@ class Classifier:
         end_eeg = layouts[self.board_id]["eeg_end"]
         eeg = data[start_eeg:end_eeg]
         trigger = data[-1]
-        raw = convert_to_mne(eeg=eeg, trigger=trigger, rescale=1e6, fs=self.fs, chs=self.chs, recompute=False, transpose=False)
+        raw = convert_to_mne(eeg=eeg, trigger=trigger, rescale=1e6, fs=self.fs, chs=self.chs, recompute=False,
+                             transpose=False)
         if BoardIds.ENOPHONE_BOARD == self.board_id:
             raw, _ = set_eeg_reference(raw, ref_channels='average')
         events = find_events(raw, stim_channel='STI', initial_event=True, shortest_event=1)
-
 
         # drop events bigger than 90
         events = events[events[:, 2] < 90]
@@ -113,8 +114,7 @@ class Classifier:
         if self.mode == 'train':
             self.sequence = np.unique(events[:, 2])
         elif self.mode == 'predict':
-            logging.debug(f"Using sequence {self.sequence}")
-            logging.debug(f"Found sequence {events[:, 2]}")
+            logging.info(f"Using sequence {self.sequence}")
             trial_events = []
             # Find iteratively the same sequence of consecutive values in the events
             j = 0
@@ -127,20 +127,19 @@ class Classifier:
                 else:
                     j = 0
                     trial_events = []
-            logging.debug(f'Found trial events {np.array(trial_events)}')
+
             if len(trial_events) < len(self.sequence):
                 raise ValueError("Not enough events found for prediction")
             events = np.array(trial_events)
+            logging.info(f'Found matching events sequence: {np.array(trial_events)}')
 
         events[:, 2][events[:, 2] != 1] = 3
         try:
             ev_ids = {'NT': 3, 'T': 1}
             event_colors = {3: 'r', 1: 'g'}
-            raw.plot(events=events, event_id=ev_ids, event_color=event_colors, color='Gray', block=True, clipping=None,
-                     scalings=25e-6)
+            #raw.plot(events=events, event_id=ev_ids, event_color=event_colors, color='Gray', block=True, clipping=None, scalings=25e-6)
         except Exception as e:
             logging.error(f"Error while plotting events: {e}")
-
 
         #filtered = basic_preprocessing_pipeline(raw, lp_freq=2, hp_freq=15, notch_freqs=(50, 60), filter_length=filter_length)
         eps = make_overlapping_epochs(data=raw, events=events, tmin=self.epoch_start, tmax=self.epoch_end,
@@ -150,14 +149,13 @@ class Classifier:
         logging.info(f"Data preprocessed and epochs extracted with shape {preprocessed.shape}")
         return preprocessed, labels
 
-    def predict(self, data, proba=False):
-        return self.predict_class(data) if not proba else self.predict_probabilities(data)
-
-    def predict_class(self, data):
+    def predict(self, data, proba=False, group=False):
         """
-        Compute the class labels for the input data
-        :param data: numpy array of shape (n_samples, n_features)
-        :return: numpy array of shape (n_samples, )
+        Predict the class labels or probabilities for the input data
+        :param data: numpy array of shape (n_samples, n_channels)
+        :param proba: bool, whether to return class labels or probabilities
+        :param group: bool, whether to group the predictions by the sequence
+        :return:
         """
         try:
             X, _ = self.preprocess(data)
@@ -166,18 +164,41 @@ class Classifier:
             return
         X = X.reshape(X.shape[0], -1)
         X = self.scaler.transform(X)
+        if not group:
+            return self.predict_class(X) if not proba else self.predict_probabilities(X)
+        else:
+            return self.group_predictions(self.predict_probabilities(X))
+
+    def predict_class(self, X):
+        """
+        Compute the class labels for the input data
+        :param X: numpy array of shape (n_samples, n_features)
+        :return: numpy array of shape (n_samples, )
+        """
         return self.model.predict(X)
 
-    def predict_probabilities(self, data):
+    def predict_probabilities(self, X):
         """
         Compute the probability estimates for the input data
-        :param data: numpy array of shape (n_samples, n_features)
+        :param X: numpy array of shape (n_samples, n_features)
         :return: numpy array of shape (n_samples, n_classes)
         """
-        X, _ = self.preprocess(data)
-        X = X.reshape(X.shape[0], -1)
-        X = self.scaler.transform(X)
         return self.model.predict_proba(X)
+
+    def group_predictions(self, y, n_method='z-score'):
+        seq = self.sequence
+        y_1 = y[:, 1]
+
+        y_1 = normalize(y_1, method=n_method)
+        # Round the probabilities to 4 decimal places
+        y_1 = np.round(y_1, 4)
+
+        # Determine the class with the highest probability in y_1
+        max_prob_class = np.argmax(y_1) + 1  # Adding 1 to match 1-indexed classes
+        output = {'class': max_prob_class}
+        for idx, prediction in enumerate(seq):
+            output[str(seq[idx])] = y_1[idx]
+        return output
 
     def plot_roc_curve(self, n_splits=5):
         """ Plot the cross-validated ROC curve for the model"""
@@ -196,5 +217,4 @@ class Classifier:
             self.mode = 'predict'
         else:
             self.mode = 'train'
-        logging.info(f"Classifier mode set to {self.mode}")
-
+        logging.debug(f"Classifier mode set to {self.mode}")

utils/preprocessing.py
@@ -78,18 +78,17 @@ def extract_epochs(data: RawArray, events, ev_ids=None, reject=None, tmin: float
 def make_overlapping_epochs(data: RawArray, events, tmin: float = -0.1, tmax: float = 0.5, baseline=None, fs: int = 250):
     pre_event_samples = int(-tmin * fs)
     post_event_samples = int(tmax * fs)
-    logging.info(f'Data shape: {data.get_data().shape}')
-    # Step 3: Extract epochs manually
+    # Extract epochs manually
     epochs_data = []
     for event in events:
         start_sample = event[0] - pre_event_samples
         end_sample = event[0] + post_event_samples
         epoch = data[:, start_sample:end_sample][0]
-        logging.info(f"Epoch shape: {epoch.shape}")
+        logging.debug(f"Epoch shape: {epoch.shape}")
         epochs_data.append(epoch)
     epochs_data = np.array(epochs_data)
-    logging.info(f"Epochs shape: {epochs_data.shape}")
+    logging.debug(f"Epochs shape: {epochs_data.shape}")
 
-    # Step 4: Create an MNE EpochsArray
+    # Create an MNE EpochsArray
     info = create_info(ch_names=data.ch_names, sfreq=fs, ch_types='eeg')
     return EpochsArray(data=epochs_data, info=info, events=events, tmin=tmin, baseline=baseline)